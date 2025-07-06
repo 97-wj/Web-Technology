8470ecd648c9d044cc16fe31758e0492f17b0767
97-wj <jannatjf3268031@gmail.com>
2025-07-06 19:46:35 +0600

    Add files via upload

---
+++ HomePage.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./css/HomePage.css">
+     <script src="https://kit.fontawesome.com/927f1d16b1.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <h1 class="nav_intro">FlavorFolio</h1>
+            <div>
+                <a id="btn" href="">Home</a>
+                <a id="btn" href="">Recipes</a>
+                <a id="btn" href="">Meal Plans</a>
+                <a id="btn"href="">Shopping List</a>
+                
+            </div>
+        </nav>
+    </header>
+
+    <main>
+       
+        <section>
+
+            <h1 class="card_tittle">Our Menu</h1>
+
+            <div class="menu_card">
+                    <div class="product-card">
+                    <img src="./image/1.jpg" alt="Product Image" class="product-image">
+                    <div class="product-info">
+                        
+                        <h3 class="product-title">Steak with Wedges</h3>
+                        <p class="product-price">৳999.00</p>
+                        <a href="../View/printFriendlyView.php">
+                            <button class="add-to-cart">
+                            <span><i class="fa-solid fa-cart-shopping"></i></span> Buy Now
+                            </button>
+                        </a>
+                    </div>
+                    </div>
+
+                
+
+                <div class="product-card">
+                    <img src="./image/2.jpg" alt="Product Image" class="product-image">
+                    <div class="product-info">
+                        
+                        <h3 class="product-title">Chicken Fajtas</h3>
+                        <p class="product-price">৳499.00</p>
+                        <a href="../View/printFriendlyView.php">
+                            <button class="add-to-cart">
+                            <span><i class="fa-solid fa-cart-shopping"></i></span> Buy Now
+                            </button>
+                        </a>
+                    </div>
+                </div>
+
+                
+
+                <div class="product-card">
+                    <img src="./image/3.jpg" alt="Product Image" class="product-image">
+                    <div class="product-info">
+                        
+                        <h3 class="product-title">Cherry Tomato</h3>
+                        <p class="product-price">৳299.00</p>
+                        <a href="../View/printFriendlyView.php">
+                            <button class="add-to-cart">
+                            <span><i class="fa-solid fa-cart-shopping"></i></span> Buy Now
+                            </button>
+                        </a>
+                    </div>
+                </div>
+
+                
+
+                <div class="product-card">
+                    <img src="./image/4.jpg" alt="Product Image" class="product-image">
+                    <div class="product-info">
+                        
+                        <h3 class="product-title">Organic Asparagus</h3>
+                        <p class="product-price">৳499.00</p>
+                        <a href="../View/printFriendlyView.php">
+                            <button class="add-to-cart">
+                            <span><i class="fa-solid fa-cart-shopping"></i></span> Buy Now
+                            </button>
+                        </a>
+                    </div>
+                </div>
+
+                
+
+                    <div class="product-card">
+                    <img src="./image/5.jpg" alt="Product Image" class="product-image">
+                    <div class="product-info">
+                        
+                        <h3 class="product-title">Chicken Roll</h3>
+                        <p class="product-price">৳399.00</p>
+                        <a href="../View/printFriendlyView.php">
+                            <button class="add-to-cart">
+                            <span><i class="fa-solid fa-cart-shopping"></i></span> Buy Now
+                            </button>
+                        </a>
+                    </div>
+                </div>
+
+                
+
+                <div class="product-card">
+                    <img src="./image/6.png" alt="Product Image" class="product-image">
+                    <div class="product-info">
+                        
+                        <h3 class="product-title">Chocolate Chip Cookies</h3>
+                        <p class="product-price">৳599.00</p>
+                        <a href="../View/printFriendlyView.php">
+                            <button class="add-to-cart">
+                            <span><i class="fa-solid fa-cart-shopping"></i></span> Buy Now
+                            </button>
+                        </a>
+                    </div>
+                </div>
+
+                
+
+                <div class="product-card">
+                    <img src="./image/7.png" alt="Product Image" class="product-image">
+                    <div class="product-info">
+                        
+                        <h3 class="product-title">Avocado & Egg Breakfast</h3>
+                        <p class="product-price">৳499.00</p>
+                        <a href="../View/printFriendlyView.php">
+                            <button class="add-to-cart">
+                            <span><i class="fa-solid fa-cart-shopping"></i></span> Buy Now
+                            </button>
+                        </a>
+                    </div>
+                </div>
+
+                
+
+                <div class="product-card">
+                    <img src="./image/1.jpg" alt="Product Image" class="product-image">
+                    <div class="product-info">
+                        
+                        <h3 class="product-title">Steak</h3>
+                        <p class="product-price">৳499.00</p>
+                        <a href="../View/printFriendlyView.php">
+                            <button class="add-to-cart">
+                            <span><i class="fa-solid fa-cart-shopping"></i></span> Buy Now
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+
+        </section>
+
+
+        <section>
+            <h1 class="card_tittle">Best Offer For you</h1>
+
+            <div class="offer_card">
+
+
+                <div>
+
+                    <div class="product-cards">
+                        <img src="./image/Burgers.jpg" alt="" class="product-images">
+                        <div class="product-infos">
+                            <div class="ratings">
+                                <span><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i></span> 
+                            </div>
+                            <h3 class="product-titles">PASTA</h3>
+                            <p class="product-prices">৳199.00</p>
+                        
+                        </div>
+                    </div>
+
+                    <div class="product-cards">
+                        <img src="./image/Burgers.jpg" alt="" class="product-images">
+                        <div class="product-infos">
+                            <div class="ratings">
+                                <span><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i></span> 
+                            </div>
+                            <h3 class="product-titles">PASTA</h3>
+                            <p class="product-prices">৳199.00</p>
+                        
+                        </div>
+                    </div>
+
+                    <div class="product-cards">
+                        <img src="./image/Burgers.jpg" alt="" class="product-images">
+                        <div class="product-infos">
+                            <div class="ratings">
+                                <span><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i></span> 
+                            </div>
+                            <h3 class="product-titles">PASTA</h3>
+                            <p class="product-prices">৳199.00</p>
+                        
+                        </div>
+                    </div>
+
+                </div>
+
+                <div>
+                <div class="product-card_offer">
+                        <img src="./image/1.jpg" alt="Product Image" class="product-image_offer">
+                        <div class="product-info">
+                            <div class="ratings">
+                                <span><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i></span> 
+                            </div>
+                            
+                            <h3 class="product-title">Steak with Wedges</h3>
+                            <p class="product-price">৳999.00</p>
+                            <a href="../View/printFriendlyView.php">
+                            <button class="add-to-cart">
+                            <span><i class="fa-solid fa-cart-shopping"></i></span> Buy Now
+                            </button>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+
+                    <div class="product-cards">
+                        <img src="./image/Burgers.jpg" alt="" class="product-images">
+                        <div class="product-infos">
+                            <div class="ratings">
+                                <span><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i></span> 
+                            </div>
+                            <h3 class="product-titles">PASTA</h3>
+                            <p class="product-prices">৳199.00</p>
+                        
+                        </div>
+                    </div>
+
+                    <div class="product-cards">
+                        <img src="./image/Burgers.jpg" alt="" class="product-images">
+                        <div class="product-infos">
+                            <div class="ratings">
+                                <span><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i></span> 
+                            </div>
+                            <h3 class="product-titles">PASTA</h3>
+                            <p class="product-prices">৳199.00</p>
+                        
+                        </div>
+                    </div>
+
+                    <div class="product-cards">
+                        <img src="./image/Burgers.jpg" alt="" class="product-images">
+                        <div class="product-infos">
+                            <div class="ratings">
+                                <span><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i></span> 
+                            </div>
+                            <h3 class="product-titles">PASTA</h3>
+                            <p class="product-prices">৳199.00</p>
+                        
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+
+    </main>
+
+
+    <footer class="footer">
+        <div>
+            <h1 class="nav_intro">FlavorFolio</h1>
+            <p>FlavorFolio helps you organize and share your favorite recipes while simplifying meal planning.</p>
+            <div class="social-icons">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-google"></i>
+                <i class="fa-brands fa-github"></i>
+            </div>
+        </div>
+        <div>
+            <h3>User Link</h3>
+            <p>About Us</p>
+            <p>Contact Us</p>
+            <p>Order Delivery</p>
+            <p>Terms of Services</p>
+        </div>
+
+        <div>
+            <h3>Contact Us</h3>
+            <p>Roadside Kitchen Bashundhara R/A +01797440XXX</p>
+        </div>
+    </footer>
+    
+</body>
+</html>
+
+ 
+        
+
+ 
