10618104268dbfb41334ffe0ca9e50d2dadb9110
97-wj <jannatjf3268031@gmail.com>
2025-07-06 19:48:50 +0600

    Add files via upload

---
+++ Profile.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <script src="https://kit.fontawesome.com/927f1d16b1.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../View/css/HomePage.css">
+    <link rel="stylesheet" href="../View/css/profile.css">
+    
+</head>
+<body>
+
+<header>
+        <nav class="navbar">
+            <h1 class="nav_intro">FlavorFolio</h1>
+            <div>
+                
+                <a id="btn"href="../View/admin/dashboard.php"><button class="profileBtn">Back</button></a>
+                
+            </div>
+        </nav>
+    </header>
+
+   
+
+    <main>
+       
+
+
+        <section>
+            
+
+            <div class="offer_card">
+
+
+                
+                
+                <div class="profile">
+                    <div class="product-card_offer">
+                        <img src="https://cdna.artstation.com/p/assets/images/images/084/124/296/large/matthew-blank-profile-photo-1.jpg?1737590038&dl=1" alt="Product Image" class="product-image_offer">
+                        <div class="product-info">
+                            <div class="ratings">
+                              
+                            </div>
+                            
+                            <h3 class="product-title">Name   :Shakil</h3>
+                            <p class="product-title">Email   :Shahriarshakil28@gmail.com</p>
+                            <p class="product-title">Adress  :Dhaka,Bashundara c block</p>
+                            <p class="product-title">Phone   :01797440737</p>
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+
+                    
+
+                    
+
+                    
+
+                </div>
+
+            </div>
+        </section>
+
+
+        
+
+    </main>
+
+
+    <footer class="footer">
+        <div>
+            <h1 class="nav_intro">FlavorFolio</h1>
+            <p>FlavorFolio helps you organize and share your favorite recipes while simplifying meal planning.</p>
+            <div class="social-icons">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-google"></i>
+                <i class="fa-brands fa-github"></i>
+            </div>
+        </div>
+        <div>
+            <h3>User Link</h3>
+            <p>About Us</p>
+            <p>Contact Us</p>
+            <p>Order Delivery</p>
+            <p>Terms of Services</p>
+        </div>
+
+        <div>
+            <h3>Contact Us</h3>
+            <p>Roadside Kitchen Bashundhara R/A +01797440XXX</p>
+        </div>
+    </footer>
+    
+</body>
+</html>
