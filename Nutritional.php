695606940e91a609ecba982b7ffd461a8b90946e
97-wj <jannatjf3268031@gmail.com>
2025-07-06 19:48:19 +0600

    Add files via upload

---
+++ Nutritional.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nutrition Facts</title>
+    <link rel="stylesheet" href="../View/css/Nutritional.css">
+    <link rel="stylesheet" href="../View/css/HomePage.css">
+    <link rel="stylesheet" href="../View/css/profile.css">
+       
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <h1 class="nav_intro">FlavorFolio</h1>
+            <div>
+                 <a id="btn"href="../View/admin/dashboard.php"><button class="profileBtn">Back</button></a>
+                
+            </div>
+        </nav>
+        
+    </header>
+
+    <section class="nutritional">
+
+    <div class="nutrition-facts">
+        <div class="title">Nutrition Facts</div>
+        <div class="serving-size">Serving Size 1 cup (228g)</div>
+        <div class="amount-per-serving">Amount Per Serving</div>
+        
+        <div class="calories">
+            Calories
+            <span class="calories-value">500</span>
+        </div>
+        
+        <div class="nutrient nutrient-bold">
+            Total Fat 24g <span class="daily-value">18%</span>
+        </div>
+        <div class="nutrient nutrient-indent">
+            Saturated Fat 3g <span class="daily-value">15%</span>
+        </div>
+        <div class="nutrient nutrient-indent">
+            Trans Fat 0g
+        </div>
+        
+        <div class="nutrient nutrient-bold">
+            Cholesterol 30mg <span class="daily-value">10%</span>
+        </div>
+        
+        <div class="nutrient nutrient-bold">
+            Sodium 470mg <span class="daily-value">20%</span>
+        </div>
+        
+        
+        
+       <div class="nutrient nutrient-bold thick-border">
+            Protein 5g
+        </div>
+        
+        <div class="nutrient">
+            Vitamin D 0mg <span class="daily-value">0%</span>
+        </div>
+        <div class="nutrient">
+            Calcium 20mg <span class="daily-value">2%</span>
+        </div>
+        <div class="nutrient">
+            Iron 0.6mg <span class="daily-value">4%</span>
+        </div>
+        <div class="nutrient thin-border">
+            Potassium 240mg <span class="daily-value">6%</span>
+        </div>
+        
+        <div class="note">
+            * The % Daily Value (DV) tells you how much a nutrient in a serving of food contributes to a daily diet: 2,000 calories a day is used for general nutrition advice.
+        </div>
+    </div>
+
+    </section>
+    
+</body>
+</html>
