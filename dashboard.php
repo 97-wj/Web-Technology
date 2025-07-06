7924706cfeb8e8c1dac33f226846a02956ea3523
97-wj <jannatjf3268031@gmail.com>
2025-07-06 19:51:14 +0600

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FlavorFolio</title>
+    <link rel="stylesheet" href="./css/dashboard.css">
+    <link rel="stylesheet" href="./css/HomePage.css">
+</head>
+<body>
+    <div class="container">
+        
+        <div class="sidebar">
+            <div class="logo">FlavorFolio</div>
+            <ul class="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Explore</a></li>
+                <li><a href="#">Favourite</a></li>
+                <li><a href="#">Order</a></li>
+                <li><a href="#">Message</a></li>
+                 <li><a href="#">Notification</a></li>
+                <li><a href="#">Settings</a></li>
+            </ul>
+        </div>
+
+      
+        <div class="main-content">
+            <header>
+                <h1>Explore Popular Food</h1>
+                
+                <p>Hello Jannat! Welcome back</p>
+                <input type="text" placeholder="Search for Anything">
+                
+                 
+            </header>
+            <div class="food-items">
+               
+                <div class="food-item">
+                    <img src="./image/Link (1).png" alt="Power Tikka">
+                    <p>Power Tikka</p>
+                    <span>$9.99</span>
+                </div>
+                <div class="food-item">
+                    <img src="./image/Link (2).png" alt="Butter Chicken">
+                    <p>Butter Chicken</p>
+                    <span>$10.99</span>
+                </div>
+                <div class="food-item">
+                    <img src="./image/Link (3).png" alt="Dosa Puri">
+                    <p>Dosa Puri</p>
+                    <span>$8.99</span>
+                </div>
+                <div class="food-item">
+                    <img src="./image/Link (4).png" alt="Power Tikka">
+                    <p>Power Tikka</p>
+                    <span>$9.99</span>
+                </div>
+                <div class="food-item">
+                    <img src="./image/Link (5).png" alt="Butter Chicken">
+                    <p>Butter Chicken</p>
+                    <span>$10.99</span>
+                </div>
+                <div class="food-item">
+                    <img src="./image/Link (2).png" alt="Dosa Puri">
+                    <p>Dosa Puri</p>
+                    <span>$8.99</span>
+                </div>
+                <div class="food-item">
+                    <img src="./image/Link (1).png" alt="Butter Chicken">
+                    <p>Butter Chicken</p>
+                    <span>$10.99</span>
+                </div>
+                <div class="food-item">
+                    <img src="./image/Link (2).png" alt="Dosa Puri">
+                    <p>Dosa Puri</p>
+                    <span>$8.99</span>
+                </div>
+                
+            </div>
+            <h3 id="shape">Meet Our Experts</h3>
+            <div class="meet-experts">
+                
+                <div class="expert">
+                    <img src="./image/Container (1).png" alt="Thomas Walim">
+                    <p>Dessert Specialist</p>
+                    <h4>Thomas Walim</h4>
+                </div>
+                <div class="expert">
+                    <img src="./image/Container.png" alt="James Jhonson">
+                    <p>Chef Master</p>
+                    <h4>James Jhonson</h4>
+                </div>
+            </div>
+        </div>
+
+    
+    </div>
+
+    <footer class="footer">
+        <div>
+            <h1 class="nav_intro">FlavorFolio</h1>
+            <p>FlavorFolio helps you organize and share your favorite recipes while simplifying meal planning.</p>
+            <div class="social-icons">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-google"></i>
+                <i class="fa-brands fa-github"></i>
+            </div>
+        </div>
+        <div>
+            <h3>User Link</h3>
+            <p>About Us</p>
+            <p>Contact Us</p>
+            <p>Order Delivery</p>
+            <p>Terms of Services</p>
+        </div>
+
+        <div>
+            <h3>Contact Us</h3>
+            <p>Roadside Kitchen Bashundhara R/A +01797440XXX</p>
+        </div>
+    </footer>
+</body>
+</html>
