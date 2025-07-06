18cc474c7cbec4f9796bf85fa434d2c6914c6522
97-wj <jannatjf3268031@gmail.com>
2025-07-06 19:51:34 +0600

    Add files via upload

---
+++ search.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search</title>
+    <link rel="stylesheet" href="./css/HomePage.css">
+     <script src="https://kit.fontawesome.com/927f1d16b1.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <h1 class="nav_intro">FlavorFolio</h1>
+            <div>
+                <a id="btn" href="">Home</a>
+                <a id="btn" href="">Recipes</a>
+                <a id="btn" href="">Meal Plans</a>
+                <a id="btn"href="">Shopping List</a>
+                
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <section>
+            
+            <div class="hero-content">
+                <img class="banner_img" src="./image/Banner.jpg" alt="">
+                <div class="intro">
+                        <h1>Discover & Organize 10,000+ Recipes</h1>
+                        <p>Plan meals, generate shopping lists, and cook smarter.</p>
+                                    
+                        <div class="search-container">
+                        <input type="text" placeholder="Search" id="search">     
+                    </div>
+                </div>
+            </div>      
+        </section>
+
+
+        
+
+
+        
+    </main>
+
+
+    <footer class="footer">
+        <div>
+            <h1 class="nav_intro">FlavorFolio</h1>
+            <p>FlavorFolio helps you organize and share your favorite recipes while simplifying meal planning.</p>
+            <div class="social-icons">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-google"></i>
+                <i class="fa-brands fa-github"></i>
+            </div>
+        </div>
+        <div>
+            <h3>User Link</h3>
+            <p>About Us</p>
+            <p>Contact Us</p>
+            <p>Order Delivery</p>
+            <p>Terms of Services</p>
+        </div>
+
+        <div>
+            <h3>Contact Us</h3>
+            <p>Roadside Kitchen Bashundhara R/A +01797440XXX</p>
+        </div>
+    </footer>
+    
+</body>
+</html>
+
+ 
+        
+
+ 
